b5220be2c9339ca357d606fd4fc302c2535f638d
Chimoha31 <mokochii1108@gmail.com>
2021-10-30 21:20:16 -0700

    page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import PrimaryButton from "./components/atoms/button/PrimaryButton";
-import SecondaryButton from "./components/atoms/button/SecondaryButton";
-import SearchInput from "./components/molecules/SearchInput";
-import UserCard from "./components/organism/user/UserCard";
-// import HeaderOnly from "./components/templetes/HeaderOnly";
-import DefaultLayout from './components/templetes/DefaultLayout';
+// import PrimaryButton from "./components/atoms/button/PrimaryButton";
+// import SecondaryButton from "./components/atoms/button/SecondaryButton";
+// import SearchInput from "./components/molecules/SearchInput";
+// import UserCard from "./components/organism/user/UserCard";
+// // import HeaderOnly from "./components/templetes/HeaderOnly";
+// import DefaultLayout from './components/templetes/DefaultLayout';
+import Router from '../src/router/Router';
 
 function App() {
-  const user = {
-    name: "じゃけぇ",
-    image: "https://source.unsplash.com/NE0XGVKTmcA",
-    email: "12345@example.com",
-    phone: "909-111-2222",
-    company: {
-      name: "テスト株式会社",
-    },
-    website: "https://google.com",
-  };
+  // const user = {
+  //   name: "じゃけぇ",
+  //   image: "https://source.unsplash.com/NE0XGVKTmcA",
+  //   email: "12345@example.com",
+  //   phone: "909-111-2222",
+  //   company: {
+  //     name: "テスト株式会社",
+  //   },
+  //   website: "https://google.com",
+  // };
 
   return (
-    // <div className="App">
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>Test</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user}></UserCard>
-      </DefaultLayout>
-    </BrowserRouter>
-    // </div>
+    <Router></Router>
+    // // <div className="App">
+    // <BrowserRouter>
+    //   <DefaultLayout>
+    //     <PrimaryButton>Test</PrimaryButton>
+    //     <SecondaryButton>検索</SecondaryButton>
+    //     <br />
+    //     <SearchInput />
+    //     <UserCard user={user}></UserCard>
+    //   </DefaultLayout>
+    // </BrowserRouter>
+    // // </div>
   );
 }
 export default App;
---
+++ src/components/pages/Top.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Top = () => {
+
+  return(
+    <SContainer>
+      <h2>Topページです</h2>
+ 
+    </SContainer>
+   )
+ };
+ 
+ const SContainer = styled.div`
+  text-align: center;
+ `
+
+
+export default Top;
